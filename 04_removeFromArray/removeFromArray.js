d18ba486a4a46176bd045f6045f71e5942a517dc
Lauren Hughes <lauren_hughes@outlook.com.au>
2024-02-13 17:33:57 +1100

    Solve 04_removeFromArray
    Modified the removeFromArray function so that it returns the specified
    array with the specified values deleted. This implementation passes all
    tests outlined in removeFromArray.spec.js.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...itemsToRemove) {
+    let newArray = [];
 
+    for (let element of originalArray) {
+        if (!(itemsToRemove.includes(element))) {
+            newArray.push(element);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
