6bddde7243de9513745268b72edaeb4e94250d88
Lauren Hughes <lauren_hughes@outlook.com.au>
2024-02-13 18:03:42 +1100

    Solve 05_sumAll
    Modified the sumAll function so that it returns the sum of all numbers
    between a specified start and end (inclusive). If negative numbers or
    non-numbers are specified, the function will instead return "ERROR".
    This implementation passes all tests outlined in sumAll.spec.js.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
 
+    if (typeof numOne != "number" || typeof numTwo != "number") {
+        return "ERROR";
+    }
+    
+    if (numTwo > numOne) {
+        startNum = numOne;
+        endNum = numTwo;
+    }
+    else {
+        startNum = numTwo;
+        endNum = numOne;
+    }
+
+    let sum = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
