670b72e18183d5b7ac0a405ac34e5618a1db1848
Lauren Hughes <lauren_hughes@outlook.com.au>
2024-02-13 16:26:44 +1100

    Solve 03_reverseString
    Modified the reverseString function so that it returns the specified
    string in reverse. This implementation passes all tests outlined in
    reverseString.spec.js.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let wordArray = word.split("");
+    wordArray.reverse();
 
+    let reversedWord = "";
+    for (let character of wordArray) {
+        reversedWord += character;
+    }
+    
+    return reversedWord;
 };
 
 // Do not edit below this line
