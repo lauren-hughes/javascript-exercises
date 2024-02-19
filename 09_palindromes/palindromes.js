fbbf00aa6bf159e361bbd87171b270f789052c9b
Lauren Hughes <lauren_hughes@outlook.com.au>
2024-02-19 22:31:37 +1100

    Solve 09_palindromes
    Created the palindromes function, which determines if a given string is
    a palindrome and returns true or false. This implementation passes all
    tests outlined in palindromes.spec.js.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Creates an array from the original string with any non-number and non-letter characters filtered out
+    let noPunctuation = string.toLowerCase().split("").filter((character) => ((character >= "a" && character <= "z") || (character >= 0 && character <= 9)) && character != " ");
+    // Reverses the filtered array and creates a string
+    let reversedString = noPunctuation.slice().reverse().join("");
+    let noPunctuationString = noPunctuation.join("");
+    return noPunctuationString === reversedString;
 };
 
 // Do not edit below this line
