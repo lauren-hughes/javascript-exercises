99cd9d42b5d9140fd631be5b6b09918cbab20489
Lauren Hughes <lauren_hughes@outlook.com.au>
2024-02-20 00:30:48 +1100

    Solve 12_findTheOldest
    Created the findTheOldest function, which sorts an array of people
    objects by age and returns the oldest person. The findTheOldestReduce
    function was also created, which uses the reduce method instead of the
    sort method. This second function was created after viewing the exercise
    solution in order to enhance understanding of a more elegant solution.
    Both implementations pass the tests outlined in findTheOldest.spec.js.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((person1, person2) => {
+        const person1Age = findAge(person1.yearOfBirth, person1.yearOfDeath);
+        const person2Age = findAge(person2.yearOfBirth, person2.yearOfDeath);
 
+        return person2Age - person1Age;
+    });
+    return people[0];
+};
+
+const findTheOldestReduce = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentPersonAge = findAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+        return (currentPersonAge > oldestAge) ? currentPerson : oldest;
+    });
+};
+
+const findAge = function(birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+
+    return deathYear - birthYear;
 };
 
 // Do not edit below this line
